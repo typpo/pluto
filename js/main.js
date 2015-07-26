19688bb5847217617085a25f4ab91d1929d16188
Ian Webster <ianw_git@ianww.com>
2015-07-26 01:19:31 -0700

    fix image preloading null bug

--- js/main.js
+++ js/main.js
@@ -268,7 +268,8 @@
 
   setTimeout(function preloadTextures() {
     for (var i=0; i < maps.length; i++) {
-      var im = new Image()
+      if (!maps[i].path) continue;
+      var im = new Image();
       im.src = 'images/' + maps[i].path;
     }
   }, 3000);
@@ -374,4 +375,26 @@
       sel.removeAllRanges();
     }
   }
+
+  function createText() {
+    var canvas1 = document.createElement('canvas');
+    var context1 = canvas1.getContext('2d');
+    context1.font = "Bold 40px Arial";
+    context1.fillStyle = "rgba(255,0,0,0.95)";
+    context1.fillText('Hello, world!', 0, 50);
+
+    var texture1 = new THREE.Texture(canvas1)
+    texture1.needsUpdate = true;
+
+    var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
+    material1.transparent = true;
+
+    var mesh1 = new THREE.Mesh(
+        new THREE.PlaneGeometry(canvas1.width, canvas1.height),
+        material1);
+    mesh1.position.set(0,50,0);
+    scene.add(mesh1);
+
+  }
+  createText();
 }());
